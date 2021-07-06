a90b3be00f5c7523801c813695e1677a4b32a116
ImAlejon <alejon2524@gmail.com>
2021-07-06 17:07:48 -0500

    Added the trend algorithm from the class

--- moda.js
+++ moda.js
@@ -17,8 +17,10 @@ const lista1Count = {};
 lista1.map(
     function(elemento) {
         if(lista1Count[elemento]){
+            console.log(lista1Count[elemento])
             lista1Count[elemento] += 1;
         } else {
+            console.log(lista1Count[elemento])
             lista1Count[elemento] = 1;
         }
     }
